1017e518f3c66a47a93c1e8674a99f0592b5cb4b
ajhridoy <ajhridoy461@gmail.com>
2024-03-26 16:31:23 +0600

    more section added

---
+++ swipper.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 40,
+    freeMode: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+  });
